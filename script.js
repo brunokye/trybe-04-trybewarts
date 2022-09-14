9dae471854de7181363738cd8a1bf54fd31d9a28
Bruno Ferreira <contatobrunokye@gmail.com>
2022-09-14 18:43:32 -0300

    Adiciona requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const buttonLogin = document.getElementById('button-login');
+const buttonSubmit = document.getElementById('submit-btn');
 const inputEmail = document.getElementById('login-email');
 const inputPassword = document.getElementById('login-password');
+const checkboxVerify = document.getElementById('agreement');
 
 function validateLogin() {
   if (inputEmail.value !== 'tryber@teste.com' || inputPassword.value !== '123456') {
@@ -9,5 +11,13 @@ function validateLogin() {
     alert('Olá, Tryber!');
   }
 }
+
+checkboxVerify.onchange = function test() {
+  if (this.checked) {
+    buttonSubmit.disabled = false;
+  } else {
+    buttonSubmit.disabled = true;
+  }
+};
 
 buttonLogin.addEventListener('click', validateLogin);
