5d4f95294dd56dd89a2ffe79869ffac56bc9d674
Bruno Ferreira <contatobrunokye@gmail.com>
2022-09-14 19:08:56 -0300

    Adiciona requisito 20

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const buttonSubmit = document.getElementById('submit-btn');
 const inputEmail = document.getElementById('login-email');
 const inputPassword = document.getElementById('login-password');
 const checkboxVerify = document.getElementById('agreement');
+const characterCounter = document.getElementById('counter');
+const textArea = document.getElementById('textarea');
 
 function validateLogin() {
   if (inputEmail.value !== 'tryber@teste.com' || inputPassword.value !== '123456') {
@@ -20,4 +22,14 @@ checkboxVerify.onchange = function test() {
   }
 };
 
+// Contador de caracteres - https://stackabuse.com/character-counter-for-text-areas-with-vanilla-javascript/
+function calculateCounter() {
+  const maxCharacters = 500;
+  const getValue = textArea.value.length;
+  const counter = maxCharacters - getValue;
+
+  characterCounter.textContent = counter;
+}
+
 buttonLogin.addEventListener('click', validateLogin);
+textArea.addEventListener('input', calculateCounter);
